498a3fc7855e2bb7eb4c16b1a6db1a3d719cf13a
Matt Gibson <gothick@gothick.org.uk>
2015-02-01 15:26:02 +0000

    First cut of a unit test for the country_rules class.

---
+++ tests/db/country_rules_test.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ *
+* @package phpBB Extension - Acme Demo
+* @copyright (c) 2014 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace gothick\geomoderate\tests\db;
+
+require(__DIR__ . '/../../../../../../tests/test_framework/phpbb_session_test_case.php');
+
+class country_rules_test extends \phpbb_session_test_case
+{
+	/**
+	 * @var \phpbb\db\driver\driver_interface
+	 */
+	protected $db;
+
+	/**
+	 * @var \phpbb\db\tools
+	 */
+	protected $db_tools;
+
+	protected $geomoderate_table;
+
+	protected $backup_cache;
+
+	protected $session;
+
+	static protected function setup_extensions()
+	{
+		return array('gothick/geomoderate');
+	}
+
+	public function setUp()
+	{
+		global $table_prefix;
+
+		parent::setUp();
+
+		$this->db = $this->new_dbal();
+		$this->db_tools = new \phpbb\db\tools($this->db);
+		$this->geomoderate_table = $table_prefix . 'gothick_geomoderate';
+
+		$this->session = $this->session_factory->get_session($this->db);
+		global $cache, $config, $phpbb_root_path, $phpEx;
+		$this->backup_cache = $cache;
+		// Change the global cache object for this test because
+		// the mock cache object does not hit the database as is needed
+		// for this test.
+		$cache = new \phpbb\cache\service(
+				new \phpbb\cache\driver\file(),
+				$config,
+				$this->db,
+				$phpbb_root_path,
+				$phpEx
+		);
+	}
+
+	public function tearDown()
+	{
+		parent::tearDown();
+		// Set cache back to what it was before the test changed it
+		global $cache;
+		$cache = $this->backup_cache;
+	}
+
+	public function getDataSet()
+	{
+		return $this->createXMLDataSet(dirname(__FILE__) . '/fixtures/country_rules.xml');
+	}
+
+	public function test_page_rules_all_results()
+	{
+		global $cache;
+		$country_rules = new \gothick\geomoderate\rules\country_rules(
+				$this->db,
+				$cache->get_driver(),
+				$this->geomoderate_table
+			);
+		$rules = $country_rules->page_rules(100);
+		$this->assertEquals(6, sizeof($rules), 'Loaded all six rules from database.');
+
+		// First entry
+		$value = reset($rules);
+		$this->assertEquals('AD', key($rules), 'First country in page should be Andorra.');
+		$this->assertEquals(0, $value, 'Andorra should be approved.');
+
+		// Last entry
+		$value = end($rules);
+		$this->assertEquals('MY', key($rules), 'Last country in page should be Malaysia.');
+		$this->assertEquals(1, $value, "Malaysia should be moderated.");
+	}
+
+	public function test_page_rules_partial_results()
+	{
+		global $cache;
+		$country_rules = new \gothick\geomoderate\rules\country_rules(
+				$this->db,
+				$cache->get_driver(),
+				$this->geomoderate_table
+		);
+		$rules = $country_rules->page_rules(4, 1);
+		$this->assertEquals(4, sizeof($rules), 'Loaded a page of four rules from database.');
+
+		// First entry
+		$value = reset($rules);
+		$this->assertEquals('AF', key($rules), 'First country in page should be Afghanistan.');
+		$this->assertEquals(0, $value, 'Afghanistan should be approved.');
+
+		// Last entry
+		$value = end($rules);
+		$this->assertEquals('MX', key($rules), 'Last country in page should be Mexico.');
+		$this->assertEquals(1, $value, "Mexico should be moderated.");
+	}
+
+	public function test_page_rules_count()
+	{
+		global $cache;
+		$country_rules = new \gothick\geomoderate\rules\country_rules(
+				$this->db,
+				$cache->get_driver(),
+				$this->geomoderate_table
+		);
+		$count = $country_rules->total_count();
+		$this->assertEquals(6, $count, 'total_count found exactly six rules.');
+	}
+
+	public function test_page_rules_should_moderate()
+	{
+		global $cache;
+		$country_rules = new \gothick\geomoderate\rules\country_rules(
+				$this->db,
+				$cache->get_driver(),
+				$this->geomoderate_table
+		);
+		$this->assertFalse($country_rules->should_moderate('AG'), 'Antigua and Barbuda should be allowed.');
+		$this->assertTrue($country_rules->should_moderate('MW'), 'Malawi should be moderated.');
+		$this->assertFalse($country_rules->should_moderate('XX'), 'Unknnown country codes should be allowed.');
+	}
+	public function test_page_rules_bulk_update()
+	{
+		global $cache;
+		$country_rules = new \gothick\geomoderate\rules\country_rules(
+				$this->db,
+				$cache->get_driver(),
+				$this->geomoderate_table
+		);
+
+		$moderate_array = array(
+			'AD' => 0,
+			'AF' => 1,
+			'AG' => 0,
+			'MW' => 1,
+			'MX' => 0,
+			'MY' => 1
+		);
+		$country_rules->bulk_update($moderate_array);
+		$expected = $this->createXMLDataSet(dirname(__FILE__) . '/fixtures/country_rules_after_update.xml')->getTable($this->geomoderate_table);
+		$query_table = $this->getConnection()->createQueryTable($this->geomoderate_table, 'SELECT * FROM ' . $this->geomoderate_table);
+		$this->assertTablesEqual($expected, $query_table);
+	}
+}
