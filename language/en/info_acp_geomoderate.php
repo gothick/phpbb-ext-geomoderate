c1ce08f70dd1952ccf071e760c21f8751355c3cd
Matt Gibson <gothick@gothick.org.uk>
2015-02-02 23:11:41 +0000

    Add missing language entry for "settings changed" log message.

--- language/en/info_acp_geomoderate.php
+++ language/en/info_acp_geomoderate.php
@@ -27,4 +27,5 @@ $lang = array_merge($lang,
 				'GEOMODERATE_LOG_LOOKUP_FAILED' => '<strong>GeoModerate country lookup failed</strong><br />» GeoIP2 Reader returned: "%1$s"',
 				'GEOMODERATE_LOG_POST_DISAPPROVED' => '<strong>Disapproved post “%1$s” written by “%3$s” for the following reason</strong><br />» Post from a moderation country (%2$s) detected by GeoModerate',
 				'GEOMODERATE_LOG_TOPIC_DISAPPROVED' => '<strong>Disapproved topic “%1$s” written by “%3$s” for the following reason</strong><br />» Topic from a moderation country (%2$s) detected by GeoModerate',
+				'GEOMODERATE_LOG_SETTING_CHANGED' => '<strong>GeoModerate settings updated.</strong>'
 		));
